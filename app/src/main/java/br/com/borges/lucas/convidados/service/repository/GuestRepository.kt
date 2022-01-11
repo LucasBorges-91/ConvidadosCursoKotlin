41d181b360436fe733c16bee2e19d4dc080ecf3f
Lucas Borges <lucas.pborges@yahoo.com.br>
2022-01-10 22:13:24 -0300

    adicionando o padrao singleton ao projeto para gerenciamento do banco de dados

--- app/src/main/java/br/com/borges/lucas/convidados/service/repository/GuestRepository.kt
+++ app/src/main/java/br/com/borges/lucas/convidados/service/repository/GuestRepository.kt
@@ -1,8 +1,25 @@
 package br.com.borges.lucas.convidados.service.repository
 
+import android.content.Context
 import br.com.borges.lucas.convidados.service.model.GuestModel
 
-class GuestRepository {
+class GuestRepository private constructor( context: Context ) {
+
+  //singleton
+
+  private var mGuestDataBaseHelper: GuestDataBaseHelper = GuestDataBaseHelper( context )
+
+  companion object {
+    private lateinit var repository: GuestRepository
+
+    fun getInstance( context: Context ): GuestRepository {
+      if ( !::repository.isInitialized ) {
+        return GuestRepository( context )
+      }
+      return repository
+    }
+  }
+
 
   fun getAll(): List<GuestModel> {
     val list: MutableList<GuestModel> = ArrayList()
@@ -20,6 +37,7 @@ class GuestRepository {
   }
 
   fun save( guest: GuestModel) {
+    
   }
 
   fun update( guest: GuestModel) {
