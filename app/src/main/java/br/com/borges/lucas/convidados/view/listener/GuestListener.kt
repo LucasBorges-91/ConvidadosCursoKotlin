585c30f970f921f4565b97f16989346354ccce7b
Lucas Borges <lucas.pborges@yahoo.com.br>
2022-01-12 17:22:11 -0300

    adicionando o metodo delete para deletar um convidade e adicionando lista de strings.

--- app/src/main/java/br/com/borges/lucas/convidados/view/listener/GuestListener.kt
+++ app/src/main/java/br/com/borges/lucas/convidados/view/listener/GuestListener.kt
@@ -2,4 +2,5 @@ package br.com.borges.lucas.convidados.view.listener
 
 interface GuestListener {
   fun onClick( id: Int )
+  fun onDelete( id: Int )
 }
--- app/src/main/java/br/com/borges/lucas/convidados/view/viewholder/GuestViewHolder.kt
+++ app/src/main/java/br/com/borges/lucas/convidados/view/viewholder/GuestViewHolder.kt
@@ -1,5 +1,6 @@
 package br.com.borges.lucas.convidados.view.viewholder
 
+import android.app.AlertDialog
 import android.view.View
 import android.widget.TextView
 import androidx.appcompat.view.menu.ActionMenuItemView
@@ -8,15 +9,28 @@ import br.com.borges.lucas.convidados.R
 import br.com.borges.lucas.convidados.service.model.GuestModel
 import br.com.borges.lucas.convidados.view.listener.GuestListener
 
-class GuestViewHolder( itemView: View, private val listener: GuestListener ) : RecyclerView.ViewHolder( itemView ) {
+class GuestViewHolder(itemView: View, private val listener: GuestListener) :
+  RecyclerView.ViewHolder(itemView) {
   //guarda as referencias dos elementos de layout
 
-  fun bind( guest: GuestModel ) {
+  fun bind(guest: GuestModel) {
     val textName = itemView.findViewById<TextView>(R.id.text_name)
     textName.text = guest.name
 
-    textName.setOnClickListener{
-      listener.onClick( guest.id )
+    textName.setOnClickListener {
+      listener.onClick(guest.id)
+    }
+
+    textName.setOnLongClickListener {
+      AlertDialog.Builder(itemView.context)
+        .setTitle(R.string.remocao_convidado)
+        .setMessage(R.string.deseja_remover)
+        .setPositiveButton(R.string.remover) { dialog, which ->
+          listener.onDelete(guest.id)
+        }
+        .setNeutralButton(R.string.cancelar, null)
+        .show()
+      true
     }
   }
 }
