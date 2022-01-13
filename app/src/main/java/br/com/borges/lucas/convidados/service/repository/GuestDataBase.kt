af66044cdbada09d3422a3ef32da263ab3ef274f
Lucas Borges <lucas.pborges@yahoo.com.br>
2022-01-13 19:32:57 -0300

    wip Iniciando a implementacao do room, modificando o guestModel para um entity e criando a classe guestDataBase.

---
+++ app/src/main/java/br/com/borges/lucas/convidados/service/repository/GuestDataBase.kt
@@ -0,0 +1,20 @@
+package br.com.borges.lucas.convidados.service.repository
+
+import android.content.Context
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+abstract class GuestDataBase : RoomDatabase() {
+
+  companion object {
+    private lateinit var INSTANCE: GuestDataBase
+    fun getDataBase( context: Context ): GuestDataBase {
+      if ( !::INSTANCE.isInitialized ) {
+        INSTANCE = Room.databaseBuilder( context, GuestDataBase::class.java, "guestDB" )
+          .allowMainThreadQueries()
+          .build()
+      }
+      return INSTANCE
+    }
+  }
+}
